dd78159061e487a0dcf5a147d992e62bf4050cdb
Evan Wieland <evwiel1@gmail.com>
2022-02-26 12:22:32 -0600

    Finsihed 5A

---
+++ PennyPixel_2DTilemapProject/Assets/PennyPixel/Scripts/Goal.cs
@@ -0,0 +1,36 @@
+﻿/*
+ * Evan Wieland
+ * Assignment 5A
+ * 
+ * Controls the goal
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Goal : MonoBehaviour
+{
+	[Header("References")]
+	public GameObject gemVisuals;
+	public CircleCollider2D gemCollider2D;
+    public Text winText;
+    public Text scoreText;
+
+	void OnTriggerEnter2D(Collider2D theCollider)
+	{
+		if (theCollider.CompareTag ("Player")) {
+			GemCollected ();
+		}
+	}
+
+	void GemCollected()
+	{
+		gemCollider2D.enabled = false;
+		gameObject.SetActive(false);
+        ScoreController.score++;
+		scoreText.text = "Score: " + ScoreController.score;
+		winText.enabled = true;
+	}
+}

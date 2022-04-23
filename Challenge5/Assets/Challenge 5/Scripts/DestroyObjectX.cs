040231396a90c054ea29ed99e2ac3fb2f64e52cb
Evan Wieland <evwiel1@gmail.com>
2022-04-23 12:30:46 -0500

    Assingment 8

---
+++ Challenge5/Assets/Challenge 5/Scripts/DestroyObjectX.cs
@@ -0,0 +1,20 @@
+﻿/*
+ * Evan Wieland
+ * Challange 8
+ * 
+ * Object destroyer
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyObjectX : MonoBehaviour
+{
+    void Start()
+    {
+        Destroy(gameObject, 2); // destroy particle after 2 seconds
+    }
+
+
+}
---
+++ Prototype5/Assets/Scripts/GameManager.cs
@@ -0,0 +1,84 @@
+﻿/*
+ * Evan Wieland
+ * Prototype 8
+ * 
+ * Game manager
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public List<GameObject> targets;
+
+    private float spawbRate = 1.0f;
+
+    public TextMeshProUGUI scoreText;
+
+    private int score;
+
+    public TextMeshProUGUI gameOverText;
+
+    public bool isGameActive;
+
+    public Button restartButton;
+
+    public GameObject titleScreen;
+    
+    public void StartGame(int difficulty)
+    {
+        spawbRate /= difficulty;
+        isGameActive = true;
+        StartCoroutine(SpawnTarget());
+        score = 0;
+        UpdateScore(0);
+        titleScreen.gameObject.SetActive(false);
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    public void GameOver()
+    {
+        isGameActive = false;
+        gameOverText.gameObject.SetActive(true);
+        restartButton.gameObject.SetActive(true);
+    }
+
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "Score: " + score;
+    }
+
+    IEnumerator SpawnTarget()
+    {
+        while (isGameActive)
+        {
+            yield return new WaitForSeconds(spawbRate);
+            
+            int index = Random.Range(0, targets.Count);
+            GameObject target = targets[index];
+            Instantiate(target);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
